bbe3f78489ce3759aa445a4641949a92b9bea0c0
lilik supriyadi <lilik.supriyadi@axiatadigitallabs.com-at-733556464980>
2023-11-06 10:36:23 +0700

    add deployment

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "aws_instance" "web_instance" {
   #!/bin/bash -ex
 
   amazon-linux-extras install nginx1 -y
-  echo "<h1>$(curl https://api.kanye.rest/?format=text)</h1>" >  /usr/share/nginx/html/index.html 
+  echo "<h1>$(curl https://api.snyk.rest/?format=text)</h1>" >  /usr/share/nginx/html/index.html 
   systemctl enable nginx
   systemctl start nginx
   EOF
